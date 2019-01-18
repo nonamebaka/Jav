db839882f68702af74fb0596e3805799f5c9d8de
Liemnht <liemnht@imt-soft.com>
2019-01-18 17:12:35 +0700

    Now

---
+++ InternTraining_Now/InternTraining/ViewController.m
@@ -0,0 +1,36 @@
+//
+//  ViewController.m
+//  InternTraining
+//
+//  Created by Cong Nguyen on 16/01/2019.
+//  Copyright © 2019 Cong Nguyen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+   
+    
+}
+
+
+- (IBAction)tvEmail:(id)sender {
+}
+
+- (IBAction)tvPass:(id)sender {
+}
+@end
