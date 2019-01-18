1dcc6339d4e0ebbcb37ea8d028f76952ca6a0475
Liemnht <liemnht@imt-soft.com>
2019-01-18 09:52:44 +0700

    Add Trainning Project

---
+++ InternTraining/InternTraining/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  InternTraining
+//
+//  Created by Cong Nguyen on 16/01/2019.
+//  Copyright © 2019 Cong Nguyen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+   
+    
+}
+
+
+@end
